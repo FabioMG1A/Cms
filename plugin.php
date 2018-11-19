558bb4732d6360df09f875bc8e111e18c189d95b
FabioMG1A <34440826+FabioMG1A@users.noreply.github.com>
2018-11-19 11:30:20 +0100

    Add files via upload

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Fabioblabla Plugin
+Plugin URI:   24675.hosts1.ma-web.nl/bewijzenmap/ma/portfolio/Portfolio
+Description:  Dani's description is best wel slecht geef mij maar 500 euro
+Version:      6969420
+Author:       Fabio van den Broek
+Author URI:   24675.hosts1.ma-web.nl/bewijzenmap/ma/portfolio/Portfolio
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
